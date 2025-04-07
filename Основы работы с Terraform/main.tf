f2afd1ce98a829811a4722c1713d59654d183bd2
vapolushkina <121248099+vapolushkina@users.noreply.github.com>
2025-04-07 19:12:08 +0300

    Update main.tf

--- Основы работы с Terraform/main.tf
+++ Основы работы с Terraform/main.tf
@@ -1,4 +1,15 @@
 ```
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
 data "yandex_compute_image" "ubuntu" {
   family = var.vm_web_family
 }
@@ -11,4 +22,66 @@ resource "yandex_compute_instance" "platform" {
     memory        = var.vm_web_memory
     core_fraction = var.vm_web_core_fraction
   }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+}
+resource "yandex_vpc_network" "develop1" {
+  name = var.vpc_name1
+}
+
+resource "yandex_vpc_subnet" "develop1" {
+  name           = var.vpc_name1
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr1
+}
+
+
+data "yandex_compute_image" "ubuntu2" {
+  family = var.vm_db_family
+}
+
+resource "yandex_compute_instance" "platform2" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop1.id
+    nat       = true
+  }
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+}
 ```
